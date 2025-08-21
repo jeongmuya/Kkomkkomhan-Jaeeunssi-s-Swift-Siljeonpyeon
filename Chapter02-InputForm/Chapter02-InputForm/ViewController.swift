58fd247b4d9ed78dbf9f4feb6962f627fb618e72
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-21 19:11:57 +0900

    Revert ":seedling: 3.1.1 UI 커스터마이징(2) - TabBar"
    This reverts commit f0a9040b38942e8723f5caf24ddb5ce06c20a8e7.

--- Chapter02-InputForm/Chapter02-InputForm/ViewController.swift
+++ Chapter02-InputForm/Chapter02-InputForm/ViewController.swift
@@ -18,9 +18,6 @@ class ViewController: UIViewController {
     var txtUpdate: UILabel! // 스위치 컨트롤의 값을 표현할 레이블
     var txtInterval: UILabel! // 스테퍼 컨트롤의 값을 표현할 레이블
     
-    // 커스텀 폰트
-    let customFont = UIFont(name: "RobotoMono-Bold", size: 14)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,37 +26,32 @@ class ViewController: UIViewController {
         
         // 2. 이메일 레이블을 생성하고 영역과 기본 문구를 설정한다.
         let lblEmail = UILabel()
-        lblEmail.frame = CGRect(x: 27, y: 150, width: 100, height: 30)
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
         lblEmail.text = "이메일"
         
         // 3. 레이블의 폰트를 설정한다.
-//        lblEmail.font = UIFont.systemFont(ofSize: 14)
-        lblEmail.font = customFont
+        lblEmail.font = UIFont.systemFont(ofSize: 14)
         
         // 4. 레이블을 루트 뷰에 추가한다.
         self.view.addSubview(lblEmail)
         
         // 자동갱신 레이블을 생성하고 루트 뷰에 추가한다.
         let lblUpdate = UILabel()
-        lblUpdate.frame = CGRect(x: lblEmail.frame.origin.x, y: 200, width: 100, height: 30)
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
         lblUpdate.text = "자동갱신"
-//        lblUpdate.font = UIFont.systemFont(ofSize: 14)
-        lblUpdate.font = customFont
-        
+        lblUpdate.font = UIFont.systemFont(ofSize: 14)
         self.view.addSubview(lblUpdate)
         
         // 갱신주기 레이블을 생성하고 루트 뷰에 추가한다.
         let lblInterval = UILabel()
-        lblInterval.frame = CGRect(x: lblEmail.frame.origin.x, y: 250, width: 100, height: 30)
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
         lblInterval.text = "갱신주기"
-//        lblInterval.font = UIFont.systemFont(ofSize: 14)
-        lblInterval.font = customFont
-        
+        lblInterval.font = UIFont.systemFont(ofSize: 14)
         self.view.addSubview(lblInterval)
         
         // 이메일 입력을 위한 텍스트 필드를 추가한다.
         self.paramEmail = UITextField()
-        self.paramEmail.frame = CGRect(x: 120, y: 150, width: 220, height: 30)
+        self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
         self.paramEmail.font = UIFont.systemFont(ofSize: 13)
         self.paramEmail.borderStyle = UITextField.BorderStyle.roundedRect
         self.paramEmail.autocapitalizationType = .none // <- 대소문자 자동 변환 가능을 해제하는 문구
@@ -67,7 +59,7 @@ class ViewController: UIViewController {
         
         // 스위치 객체를 생성한다.
         self.paramUpdate = UISwitch()
-        self.paramUpdate.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
         
         
         // 스위치가 On 되어 있는 상태를 기본값으로 설정한다.
@@ -77,18 +69,18 @@ class ViewController: UIViewController {
         // 갱신주기를 위한 스테퍼를 추가한다.
         self.paramInterval = UIStepper()
         
-        self.paramInterval.frame = CGRect(x: 120, y: 250, width: 50, height: 30)
-        self.paramInterval.minimumValue = 1 // 1. 스테퍼가 가질 수 있는 최소값
+        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.minimumValue = 0 // 1. 스테퍼가 가질 수 있는 최소값
         self.paramInterval.maximumValue = 100 // 2. 스테퍼가 가질 수 있는 최대값
         self.paramInterval.stepValue = 1 // 3. 스테퍼의 값 변경 단위
-        self.paramInterval.value = 1 // 4. 초기값 설정
+        self.paramInterval.value = 0 // 4. 초기값 설정
         
         self.view.addSubview(self.paramInterval)
         
         // 스위치 객체의 값을 표현할 레이블을 추가한다.
         self.txtUpdate = UILabel()
         
-        self.txtUpdate.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.txtUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
         self.txtUpdate.font = UIFont.systemFont(ofSize: 12)
         self.txtUpdate.textColor = UIColor.red // 1. 텍스트의 색상 설정
         self.txtUpdate.text = "갱신함" // 2. "갱신함" or "갱신하지 않음"
@@ -97,10 +89,10 @@ class ViewController: UIViewController {
         
         // 스테퍼의 값을 텍스트로 표현할 레이블을 추가한다.
         self.txtInterval = UILabel()
-        self.txtInterval.frame = CGRect(x: 250, y: 250, width: 100, height: 30)
+        self.txtInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
         self.txtInterval.font = UIFont.systemFont(ofSize: 12)
         self.txtInterval.textColor = UIColor.red
-        self.txtInterval.text = "1분마다"
+        self.txtInterval.text = "0분마다"
         
         self.view.addSubview(self.txtInterval)
         
