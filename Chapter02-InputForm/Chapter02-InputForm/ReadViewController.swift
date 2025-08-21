2e722582d32ccbe260491262958ed287749d3900
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-21 15:38:39 +0900

    2.3.5 :sparkles: 입력폼 구현 - 화면 전환 및 값 전달하기

---
+++ Chapter02-InputForm/Chapter02-InputForm/ReadViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReadViewController.swift
+//  Chapter02-InputForm
+//
+//  Created by YangJeongMu on 8/21/25.
+//
+
+import UIKit
+
+class ReadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
