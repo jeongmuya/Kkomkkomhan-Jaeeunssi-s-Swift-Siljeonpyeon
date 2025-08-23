1a252bdbecafa7e38d37ba7d1bf6de945d6a3b91
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-23 18:31:22 +0900

    ✨ 3.3.4 콘텐츠 뷰 컨트롤러 영역 확인하기

--- Chapter03-Alert/Chapter03-Alert/ViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     }
     @objc func defaultAlert(_ sender:Any) {
         // 1. 알림창을 정의한다.
-        let alert = UIAlertController(title: "알림창", message: "기본 메시지가 들어가는 곳", preferredStyle: .alert)
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
         // 2. 버튼을 정의한다.
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
@@ -30,6 +30,13 @@ class ViewController: UIViewController {
         // 3. 버튼을 알림창에 추가한다.
         alert.addAction(cancelAction)
         alert.addAction(okAction)
+        
+        // 추가된 코드) 알림창에 들어갈 뷰 컨트롤러
+        let v = UIViewController()
+        v.view.backgroundColor = UIColor.red
+        
+        // 알림창에 뷰 컨트롤러를 등록한다.
+        alert.setValue(v, forKey: "contentViewController")
         
         // 4. 알림창을 화면에 표시힌다.
         self.present(alert, animated: false)
