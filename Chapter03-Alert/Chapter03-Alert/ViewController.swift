13a9762f341fc49d8a411e2945ad279dfb2fd8c5
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-23 18:12:24 +0900

    ✨  3.3.3 프로젝트 생성 및 기본 화면 구성 - 알림창

--- Chapter03-Alert/Chapter03-Alert/ViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/ViewController.swift
@@ -11,7 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 기본 알림창 버튼 생성
+        let defaultAlertBtn = UIButton(type: .system)
+        defaultAlertBtn.frame = CGRect(x: 0, y: 100, width: 100, height: 30)
+        defaultAlertBtn.center.x = self.view.frame.width / 2
+        defaultAlertBtn.setTitle("기본 알림창", for: .normal)
+        defaultAlertBtn.addTarget(self, action: #selector(defaultAlert(_:)), for: .touchUpInside)
+        self.view.addSubview(defaultAlertBtn)
+    }
+    @objc func defaultAlert(_ sender:Any) {
+        // 1. 알림창을 정의한다.
+        let alert = UIAlertController(title: "알림창", message: "기본 메시지가 들어가는 곳", preferredStyle: .alert)
+        
+        // 2. 버튼을 정의한다.
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        // 3. 버튼을 알림창에 추가한다.
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        
+        // 4. 알림창을 화면에 표시힌다.
+        self.present(alert, animated: false)
+        
+        
     }
 
 
