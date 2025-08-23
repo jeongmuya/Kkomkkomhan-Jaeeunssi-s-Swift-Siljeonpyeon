3cb34f6015ead8044e2a2ed5f07467c264351f4a
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-23 20:36:26 +0900

    :recycle: 3.3.5 맵을 넣은 알림창 만들기

---
+++ Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapAlertViewController.swift
+//  Chapter03-Alert
+//
+//  Created by YangJeongMu on 8/23/25.
+//
+
+import UIKit
+
+class MapAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
